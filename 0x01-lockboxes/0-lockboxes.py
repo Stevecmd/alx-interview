ad65a51e113a7e5e2d3c4103e6ca08fc55544267
SteveCmd <murimi101@gmail.com>
2024-10-08 09:12:23 +0300

    Refactor lockboxes module documentation to improve code readability and add module documentation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This module contains a function that determines if all boxes can be opened.
+"""
 
 def canUnlockAll(boxes):
     """Determines if all boxes can be opened."""
